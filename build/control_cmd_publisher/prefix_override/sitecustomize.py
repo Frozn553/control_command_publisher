e75c62d8cfa81decc054105701a9a3b980de52d6
Frozn553 <mr.dasimadog@gmail.com>
2024-11-06 17:58:07 +0800

    Initial commit

---
+++ build/control_cmd_publisher/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/frozn/control_cmd_publisher/install/control_cmd_publisher'
---
+++ build/control_cmd_publisher/share/control_cmd_publisher/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/frozn/control_cmd_publisher/build/control_cmd_publisher"
